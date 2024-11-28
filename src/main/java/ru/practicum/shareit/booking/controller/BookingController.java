8c17c28cc4fbac5526b1d94c46f05f1863693b6b
Andrey Brashchenko <aab90@yandex.ru>
2024-11-28 22:29:17 +0300

    sprint 15: another try

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -17,35 +17,35 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
-    public BookingDto addBooking(@RequestHeader("${shareit.header.owner}") Integer bookerId,
+    public BookingDto addBooking(@RequestHeader("X-Sharer-User-Id") Integer bookerId,
                                  @RequestBody BookingDto bookingDto) {
         log.info("POST /bookings {} is accessed by bookerId: {}", bookingDto, bookerId);
         return bookingService.addBooking(bookerId, bookingDto);
     }
 
     @PatchMapping("{bookingId}") //?approved={approved}
-    public BookingDto approveBooking(@RequestHeader("${shareit.header.owner}") Integer ownerId,
+    public BookingDto approveBooking(@RequestHeader("X-Sharer-User-Id") Integer ownerId,
                                      @PathVariable Integer bookingId, @RequestParam Boolean approved) {
         log.info("PATCH /bookings/{}?approved={} is accessed", bookingId, approved);
         return bookingService.approveBooking(ownerId, bookingId, approved);
     }
 
     @GetMapping("{bookingId}")
-    public BookingDto getBookingById(@RequestHeader("${shareit.header.owner}") Integer userId,
+    public BookingDto getBookingById(@RequestHeader("X-Sharer-User-Id") Integer userId,
                                      @PathVariable Integer bookingId) {
         log.info("GET /bookings/{} is accessed", bookingId);
         return bookingService.getBookingById(userId, bookingId);
     }
 
     @GetMapping
-    public List<BookingDto> getAllBookingsByBookerId(@RequestHeader("${shareit.header.owner}") Integer bookerId,
+    public List<BookingDto> getAllBookingsByBookerId(@RequestHeader("X-Sharer-User-Id") Integer bookerId,
                                                      @RequestParam(defaultValue = "ALL") BookingState state) {
         log.info("GET /bookings?state={} with userId {} is accessed", state, bookerId);
         return bookingService.getAllBookingsByBookerId(bookerId, state);
     }
 
     @GetMapping("/owner")
-    public List<BookingDto> getAllBookingsByOwnerId(@RequestHeader("${shareit.header.owner}") Integer ownerId,
+    public List<BookingDto> getAllBookingsByOwnerId(@RequestHeader("X-Sharer-User-Id") Integer ownerId,
                                                     @RequestParam BookingState state) {
         log.info("GET /bookings/owner?state={} with userId {} is accessed", state, ownerId);
         return bookingService.getAllBookingsByOwnerId(ownerId, state);
