37af9076f6b7a70880a7e0fe42637464070f43a7
Andrey Brashchenko <aab90@yandex.ru>
2024-11-28 23:40:38 +0300

    sprint 15: another try

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -20,28 +20,28 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    public ItemDto addItem(@RequestHeader("X-Sharer-User-Id") Integer ownerId,
+    public ItemDto addItem(@RequestHeader("${shareit.header.owner}") Integer ownerId,
                            @RequestBody @Valid ItemDto itemDto) {
         log.info("POST /items is accessed: {}", itemDto);
         return itemService.addItem(ownerId, itemDto);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto editItem(@RequestHeader("X-Sharer-User-Id") Integer ownerId,
+    public ItemDto editItem(@RequestHeader("${shareit.header.owner}") Integer ownerId,
                             @PathVariable Integer itemId, @RequestBody ItemDto itemDto) {
         log.info("PATCH /items/{} is accessed: {}", itemId, itemDto);
         return itemService.editItem(ownerId, itemId, itemDto);
     }
 
     @GetMapping("/{itemId}")
-    public ItemResponseOnlyDto getItemById(@RequestHeader("X-Sharer-User-Id") Integer userId,
+    public ItemResponseOnlyDto getItemById(@RequestHeader("${shareit.header.owner}") Integer userId,
                                            @PathVariable Integer itemId) {
         log.info("PATCH /items/{} is accessed", itemId);
         return itemService.getItemById(userId, itemId);
     }
 
     @GetMapping
-    public List<ItemResponseOnlyDto> getAllItemsByOwner(@RequestHeader("X-Sharer-User-Id") Integer ownerId) {
+    public List<ItemResponseOnlyDto> getAllItemsByOwner(@RequestHeader("${shareit.header.owner}") Integer ownerId) {
         log.info("GET /items with ownerId {} is accessed", ownerId);
         return itemService.getAllItemsByOwner(ownerId);
     }
@@ -53,7 +53,7 @@ public class ItemController {
     }
 
     @PostMapping("/{itemId}/comment")
-    public CommentDto addComment(@RequestHeader("X-Sharer-User-Id") Integer authorId,
+    public CommentDto addComment(@RequestHeader("${shareit.header.owner}") Integer authorId,
                                  @PathVariable Integer itemId, @RequestBody @Valid CommentDto commentDto) {
         log.info("POST /items/{}/comment", itemId);
         return itemService.addComment(authorId, itemId, commentDto);
